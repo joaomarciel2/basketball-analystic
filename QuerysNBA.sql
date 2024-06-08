6ae57aabb58399b1019aabd763bf2ddbcfe1b5be
João Pedro Marciel <joao.marciel14@gmail.com>
2024-06-07 21:42:16 -0300

    QuerysNBA.sql

--- QuerysNBA.sql
+++ QuerysNBA.sql
@@ -45,8 +45,8 @@ S.team = P.team
 WHERE S.field_goal >= 300 AND P.games_played >= 58 -- A consulta retornará somente os jogadores que acertaram 300 ou mais arremessos de quadra
 -- e que jogaram 58 ou mais jogos.
 GROUP BY P.player_name, P.player_position, P.team, S.field_goal, S.two_points, S.three_points
-ORDER BY Effective_Field_Goal_Percent DESC
-LIMIT 15;
+ORDER BY Effective_Field_Goal_Percent DESC -- Ordernando a aparição dos dados pela melhor eficiência nos arremessos de quadra.
+LIMIT 15; -- Limitando para aparecer somente 15 linhas
 
 
 
@@ -68,7 +68,7 @@ P.player_name = S.player_name AND
 P.age = S.age AND
 P.team = S.team
 GROUP BY P.player_position
-ORDER BY points_per_position DESC;
+ORDER BY points_per_position DESC; -- Ordernando pela pontuação por posição, do maior ao menor valor.
 
 
 
@@ -84,7 +84,7 @@ INNER JOIN stats S
 ON P.id_player = S.id_player
 WHERE S.free_throws >= 125 -- A consulta retornará somente os jogadores que acertaram 125 ou mais lances livres.
 GROUP BY P.player_name, P.team, S.free_throws, S.free_throws_attempts
-ORDER BY free_throw_percent DESC;
+ORDER BY free_throw_percent DESC; -- Ordernando a aparição dos dados na melhor porcentagem nos lances livres.
 
 
 
@@ -101,7 +101,7 @@ S.player_name = P.player_name AND
 S.age = P.age AND
 S.team = P.team
 GROUP BY S.team
-ORDER BY assists_per_game DESC;
+ORDER BY assists_per_game DESC; -- Ordernando a aparição dos dados da melhor média de assistências por jogo.
 
 
 
@@ -109,14 +109,18 @@ ORDER BY assists_per_game DESC;
 SELECT 
 P.player_name,
 P.player_position,
-P.team,
+P.games_played,
 ROUND(SUM(S.points) / SUM(P.games_played), 1) AS 'points_per_game',
 ROUND(SUM(S.assists) / SUM(P.games_played), 1) AS 'assists_per_game',
 ROUND(SUM(S.blocks) / SUM(P.games_played), 1) AS 'blocks_per_game',
 ROUND(SUM(S.steals) / SUM(P.games_played), 1) AS 'steals_per_game',
 ROUND(SUM(S.field_goal) / SUM(S.field_goal_attempts) * 100, 1) AS 'field_goal_percent',
 ROUND(SUM(S.three_points) / SUM(S.three_points_attempts) * 100, 1) AS 'three_points_percent',
-ROUND(SUM(S.free_throws) / SUM(S.free_throws_attempts) * 100, 1) AS 'free_throw_percent'
+ROUND(SUM(S.free_throws) / SUM(S.free_throws_attempts) * 100, 1) AS 'free_throw_percent',
+CASE
+	WHEN P.games_played >= 58 THEN 'Qualified' -- Se o jogador tiver jogado 58 jogos ou mais, ele está qualificado na lista de pontuação da liga.
+	ELSE 'Not Qualified' -- Caso tiver jogado menos de 58 jogos, ele não está qualificado na lista de pontuação da liga.
+END AS games_qualified_for_nba_leaders
 FROM players P
 INNER JOIN stats S
 ON S.id_player = P.id_player AND
@@ -126,8 +130,8 @@ S.player_name = P.player_name AND
 S.age = P.age AND
 S.team = P.team
 WHERE P.team = 'Boston Celtics' -- A consulta retornará somente os jogadores que jogaram pelo Boston Celtics.
-GROUP BY P.player_name, P.team, P.player_position
-ORDER BY points_per_game DESC;
+GROUP BY P.player_name, P.games_played, P.player_position, games_qualified_for_nba_leaders
+ORDER BY points_per_game DESC; -- Consulta ordernada pela pontuação por jogo
 
 
 
@@ -135,14 +139,18 @@ ORDER BY points_per_game DESC;
 SELECT 
 P.player_name,
 P.player_position,
-P.team,
+P.games_played,
 ROUND(SUM(S.points) / SUM(P.games_played), 1) AS 'points_per_game',
 ROUND(SUM(S.assists) / SUM(P.games_played), 1) AS 'assists_per_game',
 ROUND(SUM(S.blocks) / SUM(P.games_played), 1) AS 'blocks_per_game',
 ROUND(SUM(S.steals) / SUM(P.games_played), 1) AS 'steals_per_game',
 ROUND(SUM(S.field_goal) / SUM(S.field_goal_attempts) * 100, 1) AS 'field_goal_percent',
 ROUND(SUM(S.three_points) / SUM(S.three_points_attempts) * 100, 1) AS 'three_points_percent',
-ROUND(SUM(S.free_throws) / SUM(S.free_throws_attempts) * 100, 1) AS 'free_throw_percent'
+ROUND(SUM(S.free_throws) / SUM(S.free_throws_attempts) * 100, 1) AS 'free_throw_percent',
+CASE
+	WHEN P.games_played >= 58 THEN 'Qualified' -- Se o jogador tiver jogado 58 jogos ou mais, ele está qualificado na lista de pontuação da liga.
+	ELSE 'Not Qualified' -- Caso tiver jogado menos de 58 jogos, ele não está qualificado na lista de pontuação da liga.
+END AS games_qualified_for_nba_leaders 
 FROM players P
 INNER JOIN stats S
 ON S.id_player = P.id_player AND
@@ -152,5 +160,5 @@ S.player_name = P.player_name AND
 S.age = P.age AND
 S.team = P.team
 WHERE P.team = 'Detroit Pistons' -- A consulta retornará somente os jogadores que jogaram pelo Detroit Pistons.
-GROUP BY P.player_name, P.team, P.player_position
-ORDER BY points_per_game DESC;
+GROUP BY P.player_name, P.games_played, P.player_position, games_qualified_for_nba_leaders
+ORDER BY points_per_game DESC; -- Consulta ordernada pela pontuação por jogo
